99956f10bdfc0e2455984ff68e520b676bb42e9d
Divisha <106154252+Divishax@users.noreply.github.com>
2024-02-25 17:18:42 +0530

    Time: 30 ms (65.43%), Space: 33.6 MB (55.01%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+    int lowerBound(vector<int>& arr,int x){
+        int start=0, end=arr.size()-1;
+        int ans=end;
+        
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            // maybe an answer
+            if(arr[mid]>=x){
+                ans=mid;
+                //look for smaller index on the left
+                end=mid-1;
+            }
+            else if(arr[mid]<x){
+                start=mid+1; // look on the right
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+    
+    vector<int> binarySearchMethod(vector<int>& arr, int k, int x){
+        int h=lowerBound(arr,x);
+        int l=h-1;
+        while(k--){
+            if(l<0){
+                h++;
+            }
+            else if(h>=arr.size()){
+                l--;
+            }
+            else if(x-arr[l]>arr[h]-x){
+                h++;
+            }
+            else{
+                l--;
+            }
+        }
+        return vector<int>(arr.begin()+l+1,arr.begin()+h);
+    }
+    
+    
     vector<int> twoPointer(vector<int>& arr, int k, int x) {
         int n=arr.size();
         int l=0;
@@ -13,13 +56,16 @@ public:
                 h--;
             }
         }
-        vector<int>ans;
-        for(int i=l;i<=h;i++){
-            ans.push_back(arr[i]);
-        }
-        return ans;
+        // vector<int>ans;
+        // for(int i=l;i<=h;i++){
+        //     ans.push_back(arr[i]);
+        // }
+        // return ans;
+        
+        return vector<int>(arr.begin()+l,arr.begin()+h+1);
     }
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        return twoPointer(arr,k,x);
+        // return twoPointer(arr,k,x);
+        return binarySearchMethod(arr,k,x);
     }
 };
